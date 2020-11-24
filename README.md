8a0a44bcb952c87417eb2588c67d78d8946caaff
Fuzzles <Fuzzles>
2020-11-23 18:55:47 -0800

    Add project files.

---
+++ README.md
@@ -0,0 +1,23 @@
+# UPFLoader-ActionSpeed
+
+
+
+Loader for ActionSpeed Skyrim UPF Patch:<br>
+https://www.nexusmods.com/skyrimspecialedition/mods/35097
+
+This loader gathers the mods from your load order that contain the records ActionSpeed wants to edit. Any mod you do not want to include, just untick in ModOrganizer before launching Synthesis.
+
+This outputs "ActionSpeedLoader.esp", which you then load in with zEdit to workaround zEdit's 255 plugin limit.
+
+Once you've created your zEdit patch, please delete the loader esp.
+
+This also outputs a dummy Synthesis.esp. Please only run this patch alone, and make a backup or rename any other "Synthesis.esp" you have.
+
+Be sure to check for conflicts in your created zEdit patch afterwards in xEdit.
+
+To use, install Synthesis and add this repo, or select it from the built-in list. <br>
+https://github.com/Noggog/Synthesis/
+
+
+Inspired by: https://www.nexusmods.com/skyrimspecialedition/mods/35012 <br>
+This loader was made due to the xEdit script missing some mods that need to be included.
---
+++ UPFLoader-ActionSpeed/Program.cs
@@ -0,0 +1,124 @@
+using DynamicData;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis;
+using Noggog;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Wabbajack.Common;
+
+namespace UPFLoaderActionSpeed
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return SynthesisPipeline.Instance.Patch<ISkyrimMod, ISkyrimModGetter>(
+                args: args,
+                patcher: RunPatch,
+                userPreferences: new UserPreferences()
+                {
+                    ActionsForEmptyArgs = new RunDefaultPatcher()
+                    {
+                        IdentifyingModKey = "ActionSpeedLoader.esp",
+                        TargetRelease = GameRelease.SkyrimSE,
+                        BlockAutomaticExit = true,
+                    }
+                });
+        }
+
+        public static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            
+            
+            //create hash set to hold mods we want
+            var modKeySet = new HashSet<ModKey>();
+
+
+            //check if ActionSpeed.esp is in load order
+            var mk = ModKey.FromNameAndExtension("ActionSpeed.esp");
+            if (!state.LoadOrder.ContainsKey(mk))
+            {
+                //warning if not, stops patch
+                System.Console.WriteLine($"Please add {mk} to your load order");
+                return;
+            }
+
+            //adds ActionSpeed.esp to your loader as a master, as it is required by zEdit
+            //patcher and this loader will not catch it otherwise. saves a manual click in zedit
+            else modKeySet.Add(mk);
+
+
+        // got it, carrying on
+        System.Console.WriteLine($"{mk} found! Loading...");
+
+
+        
+
+        //detect npc records in mods, adds mods containing those records to set
+        foreach (var npcGetter in state.LoadOrder.PriorityOrder.Npc().WinningOverrides())
+        {
+            var modKey = npcGetter.FormKey.ModKey;
+            var linkedKeys = npcGetter.LinkFormKeys.Select(l => l.ModKey);
+
+            //add keys to our hashset
+            modKeySet.Add(modKey);
+            modKeySet.Add(linkedKeys);
+        }
+
+        //make sure we get mods that make overrides to npc records
+        foreach (var npcGetter in state.LoadOrder.PriorityOrder.Npc().WinningContextOverrides(state.LinkCache))
+        {
+            //add keys to our hashset
+            modKeySet.Add(npcGetter.ModKey);
+        }               
+
+        //removes null mod names that may have been gathered
+        modKeySet.Remove(ModKey.Null);
+
+        //list collected masters
+        foreach (ModKey key in modKeySet)
+        {
+            System.Console.WriteLine(key);
+        }
+
+        //shows total number of mods we're going to have as masters
+        System.Console.WriteLine("\n" + "\n" + $"Adding {modKeySet.Count} masters to loader.");
+
+        System.Console.WriteLine("Finished" + "\n");
+
+        //getting the Skyrim data path to dump our created HeartSeekerLoader.esp, will be co-opted by mod organizer anyhow
+        var dataPath = Path.Combine(GameRelease.SkyrimSE.ToWjGame().MetaData().GameLocation().ToString(), "Data");
+
+        //gets modkey from the load order
+        var myLoadOrder = state.LoadOrder.Select(loadKey => loadKey.Key);
+
+        //takes the set of mods we've collected and adds them as masters to the esp
+        state.PatchMod.ModHeader.MasterReferences.AddRange(
+            modKeySet.Select(m => new MasterReference()
+            {
+                Master = m
+            }));
+
+
+        //special output of our esp to get around synthesis default, dummy synthesis esp still created
+        state.PatchMod.WriteToBinary(
+        Path.Combine(dataPath, "ActionSpeedLoader.esp"),
+        new BinaryWriteParameters()
+        {
+                // Don't modify the content of the masters list with what records we have inside
+                MastersListContent = BinaryWriteParameters.MastersListContentOption.NoCheck,
+
+                // Order the masters to match load order
+                //old load order getter config
+                MastersListOrdering = new BinaryWriteParameters.MastersListOrderingByLoadOrder(myLoadOrder),
+                //new mutagen 0.21.3, i've not got working yet
+                //MastersListOrdering = new BinaryWriteParameters.MastersListOrderingByLoadOrder(state.LoadOrder),
+                //Ignore default Synthesis.esp mod output name
+                ModKey = BinaryWriteParameters.ModKeyOption.NoCheck,
+
+        });
+    }
+    }
+}
