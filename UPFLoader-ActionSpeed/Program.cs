e70d57722b362611e391c05890929855b2758b83
Justin Swanson <justin.c.swanson@gmail.com>
2021-02-19 23:43:24 -0600

    Indenting fix

--- UPFLoader-ActionSpeed/Program.cs
+++ UPFLoader-ActionSpeed/Program.cs
@@ -56,73 +56,73 @@ namespace UPFLoaderActionSpeed
             else modKeySet.Add(mk);
 
 
-        // got it, carrying on
-        System.Console.WriteLine($"{mk} found! Loading...");
-        
+            // got it, carrying on
+            System.Console.WriteLine($"{mk} found! Loading...");
 
-        //detect npc records in mods, adds mods containing those records to set
-        foreach (var npcGetter in state.LoadOrder.PriorityOrder.Npc().WinningOverrides())
-        {
-            var modKey = npcGetter.FormKey.ModKey;
-            var linkedKeys = npcGetter.ContainedFormLinks.Select(l => l.FormKey.ModKey);
 
-            //add keys to our hashset
-            modKeySet.Add(modKey);
-            modKeySet.Add(linkedKeys);
-        }
-
-        //make sure we get mods that make overrides to npc records
-        foreach (var npcGetter in state.LoadOrder.PriorityOrder.Npc().WinningContextOverrides(state.LinkCache))
-        {
-            //add keys to our hashset
-            modKeySet.Add(npcGetter.ModKey);
-        }               
-
-        //removes null mod names that may have been gathered
-        modKeySet.Remove(ModKey.Null);
-
-        //list collected masters
-        foreach (ModKey key in modKeySet)
-        {
-            System.Console.WriteLine(key);
-        }
-
-        //shows total number of mods we're going to have as masters
-        System.Console.WriteLine("\n" + "\n" + $"Adding {modKeySet.Count} masters to loader.");
-
-        System.Console.WriteLine("Finished" + "\n");
-
-        //getting the Skyrim data path to dump our created HeartSeekerLoader.esp, will be co-opted by mod organizer anyhow
-        var dataPath = Path.Combine(GameRelease.SkyrimSE.ToWjGame().MetaData().GameLocation().ToString(), "Data");
-
-        //gets modkey from the load order
-        var myLoadOrder = state.LoadOrder.Select(loadKey => loadKey.Key);
-
-        //takes the set of mods we've collected and adds them as masters to the esp
-        state.PatchMod.ModHeader.MasterReferences.AddRange(
-            modKeySet.Select(m => new MasterReference()
+            //detect npc records in mods, adds mods containing those records to set
+            foreach (var npcGetter in state.LoadOrder.PriorityOrder.Npc().WinningOverrides())
             {
-                Master = m
-            }));
+                var modKey = npcGetter.FormKey.ModKey;
+                var linkedKeys = npcGetter.ContainedFormLinks.Select(l => l.FormKey.ModKey);
+
+                //add keys to our hashset
+                modKeySet.Add(modKey);
+                modKeySet.Add(linkedKeys);
+            }
+
+            //make sure we get mods that make overrides to npc records
+            foreach (var npcGetter in state.LoadOrder.PriorityOrder.Npc().WinningContextOverrides(state.LinkCache))
+            {
+                //add keys to our hashset
+                modKeySet.Add(npcGetter.ModKey);
+            }
+
+            //removes null mod names that may have been gathered
+            modKeySet.Remove(ModKey.Null);
+
+            //list collected masters
+            foreach (ModKey key in modKeySet)
+            {
+                System.Console.WriteLine(key);
+            }
+
+            //shows total number of mods we're going to have as masters
+            System.Console.WriteLine("\n" + "\n" + $"Adding {modKeySet.Count} masters to loader.");
+
+            System.Console.WriteLine("Finished" + "\n");
+
+            //getting the Skyrim data path to dump our created HeartSeekerLoader.esp, will be co-opted by mod organizer anyhow
+            var dataPath = Path.Combine(GameRelease.SkyrimSE.ToWjGame().MetaData().GameLocation().ToString(), "Data");
+
+            //gets modkey from the load order
+            var myLoadOrder = state.LoadOrder.Select(loadKey => loadKey.Key);
+
+            //takes the set of mods we've collected and adds them as masters to the esp
+            state.PatchMod.ModHeader.MasterReferences.AddRange(
+                modKeySet.Select(m => new MasterReference()
+                {
+                    Master = m
+                }));
 
 
-        //special output of our esp to get around synthesis default, dummy synthesis esp still created
-        state.PatchMod.WriteToBinary(
-        Path.Combine(dataPath, "ActionSpeedLoader.esp"),
-        new BinaryWriteParameters()
-        {
-                // Don't modify the content of the masters list with what records we have inside
-                MastersListContent = BinaryWriteParameters.MastersListContentOption.NoCheck,
+            //special output of our esp to get around synthesis default, dummy synthesis esp still created
+            state.PatchMod.WriteToBinary(
+            Path.Combine(dataPath, "ActionSpeedLoader.esp"),
+            new BinaryWriteParameters()
+            {
+            // Don't modify the content of the masters list with what records we have inside
+            MastersListContent = BinaryWriteParameters.MastersListContentOption.NoCheck,
 
-                // Order the masters to match load order
-                //old load order getter config
-                MastersListOrdering = new BinaryWriteParameters.MastersListOrderingByLoadOrder(myLoadOrder),
-                //new mutagen 0.21.3, i've not got working yet
-                //MastersListOrdering = new BinaryWriteParameters.MastersListOrderingByLoadOrder(state.LoadOrder),
-                //Ignore default Synthesis.esp mod output name
-                ModKey = BinaryWriteParameters.ModKeyOption.NoCheck,
+            // Order the masters to match load order
+            //old load order getter config
+            MastersListOrdering = new BinaryWriteParameters.MastersListOrderingByLoadOrder(myLoadOrder),
+            //new mutagen 0.21.3, i've not got working yet
+            //MastersListOrdering = new BinaryWriteParameters.MastersListOrderingByLoadOrder(state.LoadOrder),
+            //Ignore default Synthesis.esp mod output name
+            ModKey = BinaryWriteParameters.ModKeyOption.NoCheck,
 
-        });
-    }
+            });
+        }
     }
 }
